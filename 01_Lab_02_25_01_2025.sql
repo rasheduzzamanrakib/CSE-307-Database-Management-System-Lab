c71d0ff479ab17f72380af9eb1736c85cf373de8
rasheduzzaman-rakib <rasheduzzamanrakib3@gmail.com>
2025-01-25 15:50:10 +0600

    First Lab Code Added

---
+++ 01_Lab_02_25_01_2025.sql
@@ -0,0 +1,17 @@
+create database dbms;
+
+
+use dbms;
+CREATE TABLE St_Info
+(
+	roll INT,
+    name varchar(50),
+    dept VARCHAR(50)
+);
+
+INSERT INTO St_Info (roll, name, dept)
+values  (1, "asraful", "cse"),
+		(1, "rashed", "eee"),
+        (1, "gulto", "BBA");
+
+-- 
